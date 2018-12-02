9110c19b15509c277773b8d079287fa143c06bdd
Mariusz J <mariusz.jaskola.92@gmail.com>
2018-12-02 16:40:25 +0100

    last, first, prev and next work

--- src/app/passengers/passengers.component.ts
+++ src/app/passengers/passengers.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, OnChanges } from "@angular/core";
 import { DataService } from "../data.service";
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute } from "@angular/router";
 import { Observable } from "rxjs";
 
 @Component({
@@ -11,28 +11,22 @@ import { Observable } from "rxjs";
 export class PassengersComponent implements OnInit {
   passengers$: Object;
 
-
   constructor(private data: DataService, private route: ActivatedRoute) {
-    this.route.queryParams.subscribe(
-      params => {
-        console.log(params);
-        this.data.getPassengers(params.page, params.size).subscribe(data => {
-          this.passengers$ = data;
-          console.log("params")
-        });
-
-      }
-    )
+    this.route.queryParams.subscribe(params => {
+      console.log(params);
+      this.data.getPassengers(params.page, params.size).subscribe(data => {
+        this.passengers$ = data;
+        console.log(this.passengers$);
+      });
+    });
   }
 
   ngOnInit() {
     this.data.getPassengers(0, 20).subscribe(data => {
       this.passengers$ = data;
-      console.log("init")
+      console.log("init");
     });
   }
 
-  ngOnChanges(){
-
-  }
+  ngOnChanges() {}
 }
